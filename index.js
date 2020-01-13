5d0c747aa4d3b978eff0741ed219cd224bdb7c6e
Filip <lidvanlol@gmail.com>
2020-01-13 18:21:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(__dirname + “/public”));
+app.use('/', express.static('./public'));
 
 
 //initialize routes
