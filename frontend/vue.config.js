be23ee1038d832702834570706ad4c3994f45a05
Filip <lidvanlol@gmail.com>
2020-01-13 11:47:18 +0100

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pwa: {
     iconPaths: {
-       favicon32: 'public/favicon.ico',
-       favicon16: 'public/favicon.ico'
+       favicon32: 'public/favicon.png',
+       favicon16: 'public/favicon.png'
     }
   }
 }
