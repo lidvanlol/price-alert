bf681427cd0a171fb8e73445b2c98cdc5cc5fbfe
Filip <lidvanlol@gmail.com>
2020-01-13 21:25:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,16 +37,7 @@ app.use(cookieSession({
     keys: [keys.cookieSession.key]
 }))
 
-if (process.env.NODE_ENV === "production") {
 
-   app.use(express.static("public"));
-
-
-   app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "public", "index.html"));
-    });
-
-  }
 
 app.use(passport.initialize());
 app.use(passport.session());
